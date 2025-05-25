0f5e46dd56f178afc1015dc4e2467ff9775582dc
Nguyễn Dĩnh Khiêm <kelvinkaniken1910@gmail.com>
2025-05-25 22:21:42 +0700

    .

--- src/test/java/com/cs3332/data/object/order/OrderItemTest.java
+++ src/test/java/com/cs3332/data/object/order/OrderItemTest.java
@@ -7,16 +7,20 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderItemTest {
-//    @Test
-//    void testOrderItemConstructorAndGetters() {
-//        UUID productID = UUID.randomUUID();
-//        int quantity = 3;
-//        double priceAtOrder = 9.99;
-//
-//        OrderItem item = new OrderItem(productID,, quantity, priceAtOrder);
-//
-//        assertEquals(productID, item.getProductID());
-//        assertEquals(quantity, item.getQuantity());
-//        assertEquals(priceAtOrder, item.getPriceAtOrder());
-//    }
+    @Test
+    void testOrderItemConstructorAndGetters() {
+        UUID productID = UUID.randomUUID();
+        String productName = "Test Product";
+        String unit = "piece";
+        int quantity = 3;
+        double priceAtOrder = 9.99;
+
+        OrderItem item = new OrderItem(productID, productName, unit, quantity, priceAtOrder);
+
+        assertEquals(productID, item.getProductID());
+        assertEquals(productName, item.getProductName());
+        assertEquals(unit, item.getUnit());
+        assertEquals(quantity, item.getQuantity());
+        assertEquals(priceAtOrder, item.getPriceAtOrder());
+    }
 } 
